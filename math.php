d9800a4cd27151ef89eff724e9ac591023107ba9
Gateway47 <itsregalo047@gmail.com>
2021-03-27 20:13:10 +0300

    math

---
+++ math.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Math</title>
+</head>
+<body>
+    <?php
+        /*
+        pi() function returns the value of PI:
+        min() and max() functions can be used to find the lowest or highest value in a list 
+        abs() function returns the absolute (positive) value of a number:
+        sqrt() function returns the square root of a number:
+        round() function rounds a floating-point number to its nearest integer
+        rand() function generates a random number:
+
+        
+        */
+
+        echo pi()."<br>";
+        echo min(20,50)."<br>";
+        echo sqrt(64)."<br>";
+        echo round(6.6)."<br>";
+        echo rand(100,1000)."<br>";
+
+    ?>
+</body>
+</html>
